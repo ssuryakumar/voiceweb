65167f7d528984a0832549d28c1adb32db267bb9
surya <surya091192@gmail.com>
2014-01-15 14:14:34 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+    </head>
+    <body class="container">
+        <?php
+        // put your code here
+        ?>
+        <h1>Surya</h1>
+        <form>
+            <input type="text" x-webkit-speech class="form-control"  />
+        </form>
+    </body>
+</html>
